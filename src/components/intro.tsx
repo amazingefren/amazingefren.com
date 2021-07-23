057557f2dd229791db5d37fbda34882ddfd2c112
amazingefren <efrentheroker@hotmail.com>
2021-07-22 19:30:08 -0600

    mashed potato diet = mashed potato brain

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,69 +4,80 @@ import "./intro.scss";
 import { useEffect } from "react";
 
 const animate = () => {
-  console.log("something");
-  const text = ["developer", "software", "efren"];
-  const parent = document.getElementById("#intro-animation-container");
-  if (parent) {
-    text.forEach((child, i) => {
-      const node = document.createElement("span");
-      node.classList.add("iao");
-      node.setAttribute("id", (i + 1).toString());
-      const content = document.createTextNode(child);
-      node.appendChild(content);
-      parent?.appendChild(node);
-    });
-  }
   const tl = anime.timeline({ loop: false });
+  tl.add({
+    targets: `#intro-header`,
+    opacity: 1,
+    delay: 100,
+    duration: 500,
+    easing: "easeOutQuint",
+  });
   for (let i = 1; i <= 3; i++) {
-    tl.add({
-      targets: `#intro-anim-${i}`,
-      duration: 1000,
-      // opacity: 1,
-      easing: "easeOutQuint",
-      delay: 200,
-      translateY: [150, "-50%"],
-    })
-      .add(
-        {
-          targets: `#intro-anim-${i}`,
-          opacity: 1,
-          easing: "easeOutQuint",
-          duration: 1500,
-        },
-        "-=800"
-      )
-      .add(
+    tl.add(
+      {
+        targets: `#intro-anim-${i}`,
+        duration: 1000,
+        // opacity: 1,
+        easing: "easeOutQuint",
+        delay: 200,
+        translateY: [150, "-50%"],
+      },
+      "-=500"
+    ).add(
+      {
+        targets: `#intro-anim-${i}`,
+        opacity: 1,
+        easing: "easeOutQuint",
+        duration: 1500,
+      },
+      "-=1000"
+    );
+    if (i != 3) {
+      tl.add(
         {
           targets: `#intro-anim-${i}`,
           opacity: 0,
           duration: 1000,
           easing: "easeInOutQuint",
           delay: 0,
-          // translateY: -150
+          translateY: -150,
         },
-        "-=200"
+        "-=400"
       );
+    } else {
+      tl.add({
+        targets: `#intro-footer-arrow`,
+        opacity: 1,
+        duration: 1000,
+        easing: "easeOutQuint",
+        translateY: [-10, 0]
+      })
+    }
   }
 };
 
 const Intro: React.FC = () => {
   useEffect(() => {
     animate();
-  });
+  }, []);
   return (
     <div id="intro-root">
-      <div id="intro-header">amazing</div>
+      <div id="intro-header">
+        <div>amazing</div>
+      </div>
       <div id="intro-animation-container">
         <span id="intro-anim-1" className="iao">
-          developer
+          &nbsp;developer
         </span>
         <span id="intro-anim-2" className="iao">
-          software
+          &nbsp;software engineer
         </span>
         <span id="intro-anim-3" className="iao">
           efren
         </span>
+      </div>
+      <div id="intro-footer">
+        <span id="intro-footer-arrow">↓</span>
       </div>
     </div>
   );
