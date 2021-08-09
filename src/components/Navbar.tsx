a1d0d4a1036b4598aa385c0db303d2d4eb45ab84
amazingefren <efrentheroker@hotmail.com>
2021-08-08 19:31:12 -0600

    navbar link <noscript> friendly scrollling, with margins

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,49 +15,84 @@ import React, { useEffect, useState } from "react";
 import "../assets/Navbar.scss";
 
 function javascriptEnable() {
-  console.log('Javascript: Enabled')
-  console.log('Enabling Assets')
-  const assets = document.getElementsByClassName('javascript-toggle-show')
-  console.log(assets)
+  console.log("Javascript: Enabled");
+  console.log("Enabling Assets");
+  const assets = document.getElementsByClassName("javascript-toggle-show");
+  console.log(assets);
 }
 
+const scroll = (name: string)  => {
+  console.log(name)
+  document.getElementById(name)?.scrollIntoView()
+}
 
 const Links = () => {
-  const [enabled, enable] = useState(false)
-  useEffect(()=>{
-    enable(true)
-    console.log(enabled)
-  },[])
+  const [enabled, enable] = useState(false);
+  useEffect(() => {
+    enable(true);
+    console.log(enabled);
+  }, []);
   return (
     <>
       <a
-      href={!enabled ? "#skills-root": undefined}
-      onClick={enabled ? ()=>{console.log('clicked')}: undefined}
-      className="navbar-link"
-      >hi</a>
+        href={!enabled ? "#about-root" : undefined}
+        onClick={
+          enabled ? ()=>scroll("about-root") : undefined
+        }
+        className="navbar-link"
+      >
+       About
+      </a>
+      <a
+        href={!enabled ? "#education-root" : undefined}
+        onClick={
+          enabled ? ()=>scroll("education-root") : undefined
+        }
+        className="navbar-link"
+      >
+       Education 
+      </a>
+      <a
+        href={!enabled ? "#skills-root" : undefined}
+        onClick={
+          enabled ? ()=>scroll("skills-root") : undefined
+        }
+        className="navbar-link"
+      >
+       Skills 
+      </a>
+      <a
+        href={!enabled ? "#projects-root" : undefined}
+        onClick={
+          enabled ? ()=>scroll("projects-root") : undefined
+        }
+        className="navbar-link"
+      >
+       Projects
+      </a>
+      <a
+        href={!enabled ? "#contact-root" : undefined}
+        onClick={
+          enabled ? ()=>scroll("contact-root") : undefined
+        }
+        className="navbar-link"
+      >
+       Contact Me
+      </a>
     </>
-  )
-}
+  );
+};
 
 const Intro: React.FC = () => {
-  useEffect(()=>{
-    javascriptEnable()
-  }, [])
+  useEffect(() => {
+    javascriptEnable();
+  }, []);
   return (
-    <div>
-      <div className="section-root" id="header-root">
-        <div id="intro-container" className="section-container">
-          <div id="intro-header">amazingefren</div>
-          <div id="intro-supp">
-            
-            {<Links/>}
-               
-            {/* LINKS WITH / WITHOUT JAVASCRIPT
-            <noscript>
-              <a href="#skills-root">hi</a>
-            </noscript>
-            */}
-          </div>
+    <div className="section-root" id="navbar-root">
+      <div id="navbar-container" className="section-container">
+        <div id="navbar-header">amazingefren</div>
+        <div id="navbar-links">
+          <Links />
         </div>
       </div>
     </div>
