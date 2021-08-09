88421e7add08c486aeb02533bf9beadcd4e863f4
amazingefren <efrentheroker@hotmail.com>
2021-08-08 18:07:57 -0600

    quick animation, todo preloader thing maybe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import '../assets/Navbar.scss'
+import React from "react";
+import "../assets/Navbar.scss";
 
 const Navbar: React.FC = () => {
   return (
-    <div id="navbar-root"><div>☰</div></div>
-  )
-}
-export default Navbar
+    <div id="navbar-root">
+      <div>☰</div>
+    </div>
+  );
+};
+export default Navbar;
