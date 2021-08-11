43942f622de30764ceaef8481ce8428c583d3a3d
amazingefren <efrentheroker@hotmail.com>
2021-08-10 19:57:45 -0600

    projects init

---
+++ src/components/Projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Projects = () => {
+  <div>
+      Hello World
+  </div>
+
+}
+
+export default Projects;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import Navbar from './Navbar'
 import Education from './Education'
 import About from './About'
 import Links from './Links'
+import Projects from './Projects'
 
-export {Navbar, Header, Education, About, Skills, Links}
+export {Navbar, Header, Education, About, Skills, Links, Projects}
