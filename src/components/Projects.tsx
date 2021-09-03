b831fa8d4683ac654d07ff7cf9b9f84fd4727f0e
amazingefren <efrentheroker@hotmail.com>
2021-09-02 20:50:01 -0600

    basic card data models

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,96 @@
-import React from 'react'
+import React from "react";
+
+interface TechType {
+  name: string,
+  url: string
+}
+interface ProjectType {
+  name: string;
+  url: string;
+  git: string;
+  tech: TechType[];
+  description: string;
+}
+
+const Tech: {[key: string]: TechType} = {
+  nextjs: {
+    name: "NextJS",
+    url: 'hi'
+  },
+  apollo: {
+    name: "Apollo",
+    url: "idk",
+  },
+  jwtAuthentication: {
+    name: "User Authentication",
+    url: "idk",
+  },
+  typescript: {
+    name: "Typescript",
+    url: "idk",
+  },
+  node: {
+    name: "NodeJS",
+    url: "idk",
+  },
+  nestjs: {
+    name: "NestJS",
+    url: "IDK"
+  }
+};
+
+const ProjectData: ProjectType[] = [
+  {
+    name: "SOSILE CLIENT",
+    url: "https://sosile.amazingefren.com/",
+    git: "https://github.com/amazingefren/SOSILE-CLIENT",
+    tech: [
+      Tech.nextjs,
+      Tech.apollo,
+      Tech.jwtAuthentication,
+      Tech.typescript,
+      Tech.node,
+    ],
+    description: "SOSILE is the current project I am working on",
+  },
+  {
+    name: "SOSILE SERVER",
+    url: "https://sosile.amazingefren.com/",
+    git: "https://github.com/amazingefren/SOSILE-SERVER",
+    tech: [
+      Tech.nest,
+      Tech.apollo,
+      Tech.jwtAuthentication,
+      Tech.typescript,
+      Tech.node,
+    ],
+    description: "SOSILE is the current project I am working on",
+  },
+];
+
+const ProjectCard = ({ data: project }: { data: typeof ProjectData[0] }) => {
+  return (
+    <div>
+      <div>{project.name}</div>
+      <div>
+        {project.tech.map(({ name }) => (
+          <div>{name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Projects = () => {
   return (
     <div className="section-root" id="project-root">
       <div className="section-container" id="project-container">
-        <div>PROJECTS</div>
+        {ProjectData.map((project: typeof ProjectData[0]) => (
+          <ProjectCard data={project} />
+        ))}
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Projects;
